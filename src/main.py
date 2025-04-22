0f856baf37dcbdb11574a57d39d2911c6e9d9ab1
vickvey <vickvey0303@gmail.com>
2025-04-22 22:18:10 +0530

    configured allowed origins

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import stock_routes
+from dotenv import load_dotenv
+import os
+
+# Load env variables from .env file
+load_dotenv()
+
+# Read and split origins
+origins = os.getenv("ALLOWED_ORIGINS", "").split(",") + ['http://localhost']
 
 app = FastAPI()
 
-# CORS configuration for development
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(stock_routes.router, prefix="/api", tags=["stocks"])
